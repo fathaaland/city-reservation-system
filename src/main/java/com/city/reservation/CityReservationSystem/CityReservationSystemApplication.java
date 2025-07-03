44e653bb21be6e07a37287ce8a9c7a8e5f1336d6
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-07-03 20:40:16 +0200

    first commit

---
+++ src/main/java/com/city/reservation/CityReservationSystem/CityReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.city.reservation.CityReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/city/reservation/CityReservationSystem/CityReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.reservation.CityReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
