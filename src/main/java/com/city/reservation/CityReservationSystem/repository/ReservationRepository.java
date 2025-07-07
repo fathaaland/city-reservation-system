851bdc131d9a41fc5ce242ee41e9283dd7515449
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-07-07 23:31:48 +0200

    create reservation repository

--- src/main/java/com/city/reservation/CityReservationSystem/repository/ReservationRepository.java
+++ src/main/java/com/city/reservation/CityReservationSystem/repository/ReservationRepository.java
@@ -1,4 +1,10 @@
 package com.city.reservation.CityReservationSystem.repository;
 
-public interface ReservationRepository {
+import com.city.reservation.CityReservationSystem.model.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    Reservation findById(long id);
+    boolean existsById(long id);
+
 }
