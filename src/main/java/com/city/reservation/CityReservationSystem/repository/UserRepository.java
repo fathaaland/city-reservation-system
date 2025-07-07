26289d624f7f1e93d7581b65cabd079da9d4fdfe
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-07-07 23:30:57 +0200

    create user repository

--- src/main/java/com/city/reservation/CityReservationSystem/repository/UserRepository.java
+++ src/main/java/com/city/reservation/CityReservationSystem/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.city.reservation.CityReservationSystem.repository;
 
-public interface UserRepository {
+import com.city.reservation.CityReservationSystem.model.entity.SportFacility;
+import com.city.reservation.CityReservationSystem.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
 }
