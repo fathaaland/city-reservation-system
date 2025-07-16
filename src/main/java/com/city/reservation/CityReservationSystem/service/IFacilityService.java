8ed14a30eca90a445437a061315da8123e4f8eb1
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-07-16 19:54:48 +0200

    create IFacilityService, all interface is ready

--- src/main/java/com/city/reservation/CityReservationSystem/service/IFacilityService.java
+++ src/main/java/com/city/reservation/CityReservationSystem/service/IFacilityService.java
@@ -1,4 +1,14 @@
 package com.city.reservation.CityReservationSystem.service;
 
+import com.city.reservation.CityReservationSystem.model.entity.SportFacility;
+
 public interface IFacilityService {
+    SportFacility addFacility(SportFacility facility);
+    SportFacility getFacilityById(Long id);
+    void deleteFacilityById(Long id);
+    SportFacility updateFacility(Long facilityId, SportFacility facility);
+    Iterable<SportFacility> getAllFacilities();
+    Iterable<SportFacility> getFacilitiesByType(String type);
+    Iterable<SportFacility> getFacilitiesByLocation(String location);
+    Iterable<SportFacility> getFacilitiesByName(String name);
 }
