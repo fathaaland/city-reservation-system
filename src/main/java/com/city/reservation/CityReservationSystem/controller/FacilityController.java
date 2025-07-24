f29447fd57e5185308b288c49ba859f582bf4adc
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-07-24 18:20:55 +0200

    finish addFacilityController

--- src/main/java/com/city/reservation/CityReservationSystem/controller/FacilityController.java
+++ src/main/java/com/city/reservation/CityReservationSystem/controller/FacilityController.java
@@ -1,4 +1,22 @@
 package com.city.reservation.CityReservationSystem.controller;
 
+import com.city.reservation.CityReservationSystem.model.entity.SportFacility;
+import com.city.reservation.CityReservationSystem.service.FacilityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/facilities")
+@RequiredArgsConstructor
 public class FacilityController {
+
+    private final FacilityService facilityService;
+
+    @PostMapping
+    public ResponseEntity<SportFacility> addFacility(@RequestBody SportFacility facility) {
+        SportFacility createdFacility = facilityService.addFacility(facility);
+        return new ResponseEntity<>(createdFacility, HttpStatus.CREATED);
+    }
 }
