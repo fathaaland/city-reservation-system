a5666be21f1797959f0e8b1af320342a9faaa62b
David Dobrovolny <dejv.dobrovolny642001@gmail.com>
2025-10-31 16:28:21 +0100

    set name unique to facility name

--- src/main/java/com/city/reservation/CityReservationSystem/repository/FacilityRepository.java
+++ src/main/java/com/city/reservation/CityReservationSystem/repository/FacilityRepository.java
@@ -10,6 +10,4 @@ public interface FacilityRepository extends JpaRepository<SportFacility, Long> {
     boolean existsByName(String name);
 
     Iterable<SportFacility> findByType(SportType type);
-
-    boolean existsByType(SportType type);
 }
